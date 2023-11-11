45f5b6cebc2967950afdae39ef451f3145163e2b
cristian <cristiandevar@gmail.com>
2023-11-11 17:00:07 -0300

    Detallamos la reestructuracion

--- TPFinal/inc/config.php
+++ TPFinal/inc/config.php
@@ -1,6 +1,24 @@
 <?php
-    define("DB_HOST", "postgres");
-    define("DB_PORT", "5432");
-    define("DB_USERNAME", "postgres");
-    define("DB_PASSWORD", "postgres");
+
+$configFile = "./inc/credenciales.txt";
+if (!file_exists($configFile)) {
+    die("Error: El archivo de configuración $configFile no existe.");
+}
+
+// Se lee el archivo de configuración
+$config = parse_ini_file($configFile);
+
+// Se obtienen las credenciales de la base de datos desde el archivo de configuración
+$host = $config['host'];
+$port = $config['port'];
+$user = $config['user'];
+$password = $config['password'];
+
+
+// Definir constantes para su uso posterior en el código
+define("DB_HOST", $host);
+define("DB_PORT", $port);
+define("DB_USERNAME", $user);
+define("DB_PASSWORD", $password);
+
 ?>
